4e79c6b03ef077128124ed484a32e9ed8b4ba499
HugoSilva2 <hugomiguelsarmentodasilva@gmail.com>
2024-03-14 22:57:33 +0000

    GetSpecial Done

--- Special/Program.cs
+++ Special/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Threading.Tasks.Dataflow;
 
 namespace Special
 {
     class Program
     {
+        private static int GetSpecial(int n)
+        {
+            int outcome;
+
+            
+            if (n == 0)
+            {   
+                outcome = 0;
+            }
+
+            else if (n == 1)
+            {   
+                outcome = 1;
+            }
+
+            else
+            {
+                outcome = GetSpecial(n-1) + 2*GetSpecial(n-2);
+            }
+            return outcome;
+            
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("insert number of the special sequence: ");
+            string s = Console.ReadLine();
+            int n = int.Parse(s);
+            n = GetSpecial(n);
+            Console.WriteLine(n);
         }
     }
 }
