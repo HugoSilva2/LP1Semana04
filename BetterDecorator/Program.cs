039a86a0ed68c76ee38d2f561d93b933a891f1c8
HugoSilva2 <hugomiguelsarmentodasilva@gmail.com>
2024-03-14 22:03:15 +0000

    Exercise 1 Done

---
+++ BetterDecorator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BetterDecorator
+{
+    class Program
+    {
+        private static string Decor(string s, char dec, int num)
+        {
+            for(int i = 0; i < num; i++)
+            {
+                s = $"{dec}{s}{dec}";
+            }
+            return s;
+        }
+        static void Main(string[] args)
+        {
+           string s = args[0];
+           char dec = char.Parse(args[1]);
+           int num = int.Parse(args[2]);
+           s = Decor(s,dec,num);
+           Console.WriteLine(s);
+
+        }
+    }
+}
