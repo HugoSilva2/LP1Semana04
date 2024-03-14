c6f8ee365edd36ace4479f497da75eedffbafb11
HugoSilva2 <hugomiguelsarmentodasilva@gmail.com>
2024-03-14 22:10:32 +0000

    XML Comments added

--- BetterDecorator/Program.cs
+++ BetterDecorator/Program.cs
@@ -2,8 +2,21 @@
 
 namespace BetterDecorator
 {
+    /// <summary>
+    /// This program decorates a string with a chosen character
+    /// The amount of characters in each side of the string is determined by a chosen integer
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// This Method receives a string, a character and an integer
+        /// In the for loop, it adds the character before and after the string
+        /// The loop runs the amount of the integer
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="dec"></param>
+        /// <param name="num"></param>
+        /// <returns></returns>
         private static string Decor(string s, char dec, int num)
         {
             for(int i = 0; i < num; i++)
@@ -12,6 +25,14 @@ namespace BetterDecorator
             }
             return s;
         }
+        /// <summary>
+        /// It receives 3 arguments
+        /// The second argument is turned into a character
+        /// The third argument is turned into an integer
+        /// Then runs the method Decor()
+        /// Writes the outcome of the method Decor()
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
            string s = args[0];
